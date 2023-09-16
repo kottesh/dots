68faccb4cc8a586fae76ec5d748570f747bc2394
Shree Kottes <67730727+shreekottes@users.noreply.github.com>
2023-09-16 19:13:03 +0530

    update zsh config

--- config/zsh/.config/zsh/aliases.zsh
+++ config/zsh/.config/zsh/aliases.zsh
@@ -1,4 +1,39 @@
-#nvim
+#######################################
+# ALIASES                   	      #
+#######################################
+
+
+#-----------------------
+# Misc
+
+alias -s md="grip -b"
+
+#-----------------------
+# Apps
+
 alias v="nvim"
 
+
+#-----------------------
+# Navigations
+
 alias dots="cd $HOME/.dotfiles"
+
+
+#-----------------------
+# Commands 
+
+alias q="exit"
+alias sudo="sudo "
+alias jctl="journalctl -p 3 -xb" #get the error messages from journalctl
+alias p="xclip -sel clipboard -out"
+alias ex="atool --explain --extract"
+alias watch="watch -d"
+
+alias cp="cp -rv"
+alias mv="mv -v"
+alias rm="rm -rv"
+
+alias paci="sudo pacman -S"
+alias pacu="sudo pacman -Syu"
+alias pacr="sudo pacman -Rcns"
---
+++ config/zsh/.config/zsh/funcs.zsh
@@ -0,0 +1,77 @@
+# copy file contents to the clipboard 
+function y() {
+    if [[ $# -eq 0 ]]; then
+        echo "Error: No arguments provided. Usage: y <filename>"
+        return 1
+    fi
+    
+    if [[ ! -e "$1" ]]; then
+        echo "Error: File not found: $1"
+        return 1
+    fi
+    
+    if [[ -d "$1" ]]; then
+        echo "Error: $1 is a directory. Please provide a valid file."
+        return 1
+    fi
+    
+    content=$(< "$1") 
+    if [[ -z "$content" ]]; then
+        echo "Error: File is empty: $1"
+        return 1
+    fi
+    
+    echo -n "$content" | xsel --clipboard --input
+    notify-send "Copied!"
+}
+
+orphans() {
+  if [[ ! -n $(pacman -Qdt) ]]; then
+    echo "No orphans to remove."
+  else
+    sudo pacman -Rns $(pacman -Qdtq)
+  fi
+}
+
+get-ip() {
+    curl -Ss "https://ifconfig.me"
+}
+
+get-ip!() {
+    curl -Ss "https://ipapi.co/$(get-ip)/yaml" 
+}
+
+# remove symlinks and replace the original file
+rm-symlink() {
+    [[ -L "$1" ]] && cp --remove-destination "$(readlink "$1")" "$1"
+}
+
+# pretty print aliases
+aliases() {
+	alias | awk -F= '{
+		alias=$1; $1=""
+		printf "\033[34m%s\033[0m = \033[32m%s\033[0m\n", alias, $0
+	}'
+}
+
+# create a new directory and cd to it
+function mkcd {
+	mkdir -p "$1" && cd "$1"
+}
+
+# source the .zshrc
+function reload {
+	source $HOME/.config/zsh/.zshrc
+	notify-send "Reloaded zshrc"
+}
+
+# create a script file and give execute permission
+function touchx {
+	touch "$1" && chmod u+x "$1"
+}
+
+function touchs {
+    echo -e "#!/usr/bin/env bash\n\n" > "$1";
+    chmod u+x "$1";
+    nvim "$1";
+}
